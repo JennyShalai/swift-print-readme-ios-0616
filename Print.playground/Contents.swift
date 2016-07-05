23b5c0c3b1d3464efa61224449f15870a59eb016
JennyShalai <jenny.shalai@gmail.com>
2016-07-05 12:05:40 -0400

    done

--- Print.playground/Contents.swift
+++ Print.playground/Contents.swift
@@ -1,12 +1,15 @@
 var favoriteWord = "mercurial"    // Use your own favorite word!
 var favoriteColor = "blue"        // Use your own favorite color!
 var favoriteMusician = "Nirvana"  // Use your own favorite musician!
+var name = "Jenny"
 
 print(favoriteWord)
 print(favoriteColor)
 print(favoriteMusician)
+print(name)
 
 print("I'm printing a string in Swift!")
+print("\(name)'s printing a string in swift")
 
 print("Hi Mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.")
 
